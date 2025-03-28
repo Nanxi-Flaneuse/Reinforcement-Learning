6bf4a8f803f78c3a669b05e48593a309a6fa2f77
Nanxi-13 <selenananseki@gmail.com>
2025-03-28 12:03:44 -0400

    updates

---
+++ README.md
@@ -0,0 +1,4 @@
+1. libraries needed: numpy, random, matplotlib
+2. you can change the maze to see different results. The maze ws created around line 220 of the execution section of main.py
+3. To replicate the result of the experiment, use seed "5436", which is already implemented in the execution section. Run the main.py file.
+4. to change parameters, see documentation of parameters for each class
--- assignments/TD/main.py
+++ assignments/TD/main.py
@@ -1,59 +1,48 @@
 import numpy as np
 import random
-from tqdm import tqdm
 import matplotlib.pyplot as plt
-import pickle
 
 
 # target policy: keeps moving right
 # behavior policy: arbitrarily chooses between going right and left
 # moving right incurrs a reward of 1, and left -1
 
-'''debugging/TODO
-- add different n comparison
-- come up with research question: is off policy more data efficient than on policy in this example? 
-'''
 # @type can either be of values: simple, control, or on
 class TD():
     def __init__(self,step, n, seed, maze, discount, target_prob, epochs = 1000, type = 'simple') -> None:
-        self.rng = np.random.default_rng(seed)
+        self.rng = np.random.default_rng(seed) # setting the random seed
         self.state_num = len(maze)
         values = list(self.rng.normal(1, 0.01, self.state_num)) # initialize v
-        self.v = dict(zip(maze, values))
-        self.epochs = epochs
-        self.state_storage = {}
-        self.reward_storage = {}
-        # self.action_storage = {}
-        self.start = 0
+        self.v = dict(zip(maze, values)) # stores the values for each state
+        self.epochs = epochs # number of epochs
+        self.state_storage = {}# stores the state in each episode for update purposes. Cleared after each episode
+        self.reward_storage = {} # stores the reward in each episode for update purposes. Cleared after each episode
+        self.start = 0  # starting point
         self.step = step # step factor alpha when updating value functions
-        self.n = n
+        self.n = n # how large of a step the agent is taking to update its values in each episode
         self.maze = maze # the environment/states
         self.type = type # indicates which type of algorithm will be implemented - simple or control
         self.discount = discount # lambda
         self.target_prob = target_prob # probablity for the target policy to take action 'R'. P('L') = 1 - target_prob
         self.true_value = self.get_true_values() # find the true value for each state under the target policy
-        self.value_storage = []
+        self.value_storage = [] # retain the values of each episode
 
     def train(self):
         self.state_storage.clear()
         self.reward_storage.clear()
-        # self.action_storage.clear()
         for _ in range(self.epochs):
-            # print('running episode')
             T = float('inf')
             t = 0
             tau = 0
             curr_pos = 0
             self.state_storage[0] = 1
             self.reward_storage[0] = 0  # No reward at initial position
-            # self.action_storage[0] = None
             while tau != T - 1:
                 if t < T:
                     if self.type != 'on':
-                        curr_pos += 1 # QUESTION: what is curr pos exceeds the index of the maze????????
+                        curr_pos += 1 
                         self.state_storage[t+1] = self.maze[curr_pos]
                         self.reward_storage[t+1] = 1
-                        # self.action_storage[t+1] = 'R'
                     else:
                         action = self.rng.choice([1,-1],p=[self.target_prob, 1 - self.target_prob])
                         if action == -1 and curr_pos > 0:
@@ -65,9 +54,6 @@ class TD():
                         else:
                             self.reward_storage[t+1] = 0
                         self.state_storage[t+1] = self.maze[curr_pos]
-                        # self.reward_storage[t+1] = action
-                        
-                        # self.action_storage[t+1] = 'R' if action == 1 else 'L'
                     try:
                         if self.maze[curr_pos] == self.maze[-1]:
                             T = t + 1
@@ -75,30 +61,24 @@ class TD():
                         pass
                 tau = t - self.n + 1
                 if tau >= 0:
-                    # udpate G
-                    # i = tau + 1
                     G = 0
                     for i in range(tau + 1, min(tau + self.n, T) + 1):
                         G += self.discount ** (i - tau - 1) * self.reward_storage[i]
-                        # i += 1
 
                     # calculate rho
                     rho = self.target_prob/1 ** (min(tau + self.n, T) - (tau + 1) + 1) # pie/b^(n or number of steps taken before reaching terminal)
 
                     if tau + self.n < T:
-                        # if self.type == 'simple':
                         G += self.discount ** self.n * self.v[self.state_storage[tau + self.n]]
                     if self.type =='control':
                         G = self.target_prob/1 * G + (1 - self.target_prob/1) * self.v[self.state_storage[tau]]
                     
                     # update V
-                    # if self.type == 'simple':
                     if self.type != 'on':
                         self.v[self.state_storage[tau]] += self.step * rho * (G - self.v[self.state_storage[tau]])
                     else:
                         self.v[self.state_storage[tau]] += self.step * (G - self.v[self.state_storage[tau]])
-                    # else:
-                    #     self.v[self.state_storage[tau]] += self.step * rho * (G - self.v[self.state_storage[tau]])
+
                 t += 1 # update t
             # storing the values of this episode
             self.value_storage.append(list(self.v.values()))
@@ -140,7 +120,7 @@ class TD():
         
         return V
 
-    # written by chatGPT: plot the mean square error or learned values vs true value
+    # the following function is written by chatGPT: plot the mean square error or learned values vs true value
     def plot(self, error_type = 'mse', plot = False, window = 10):
         true_values = np.array(self.true_value)
         error_per_episode = []
@@ -168,7 +148,8 @@ class TD():
             plt.savefig(file) 
             # plt.show()
         return error_per_episode
-
+    
+# see documentation on the parameters in the TD class for reference
 class Experiment():
     def __init__(self,seeds, step, n, maze, discount, target_prob, epochs = 100, error_type='mse') -> None:
         self.seeds = seeds
@@ -182,7 +163,7 @@ class Experiment():
         self.simple_errors = []
         self.control_errors = []
         self.on_errors = []
-
+    # train the agents
     def train(self):
         for seed in self.seeds:
             simple = TD(self.step, self.n, seed, self.maze, self.discount, self.target_prob, self.epochs)
@@ -195,12 +176,11 @@ class Experiment():
             self.control_errors.append(control.plot(self.error_type))
             on.train()
             self.on_errors.append(on.plot(self.error_type))
-
-    def plot(self, window = 20):
+    # plotting mse error
+    def plot(self):
         average_simple = np.mean(self.simple_errors, axis=0)
         average_control = np.mean(self.control_errors, axis=0)
         average_on = np.mean(self.on_errors, axis=0)
-        # average_on = np.convolve(average_on, np.ones(window)/window, mode='valid')
         plt.plot(average_simple, label="off-policy simple method", color='b')
         plt.plot(average_control, label="off-policy control method", color='r')
         plt.plot(average_on, label="on-policy method", color='g')
@@ -219,22 +199,29 @@ class Experiment():
 
 if __name__ == '__main__':
     maze = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15] # 1 marks beginning, and 2 marks the terminal state
+    
+    # INSTRUCTION: uncomment the following three blocks to train an agent on a single algorithm
+    # simple algorithm
     # simple = TD(0.2, 2, 46288, maze, 0.8, 0.5, 20)
     # simple.train()
     # simple.plot('rms')
 
+    # control variate algorithm
     # control = TD(0.2, 2, 46288, maze, 0.8, 0.5, 20, 'control')
     # control.train()
     # control.plot('rms')
-
+    
+    # on-policy algorithm
     # on = TD(0.2, 6, 46288, maze, 0.8, 0.5, 60, 'on')
     # on.train()
     # on.plot('rms', True,30)
 
+    # generating seeds for the experiment
     random.seed(5436)
     seeds = random.sample(range(1, 10**6), 20) 
    
     
+    # INSTRUCTION: run one snippet at the time to execute one experiment. Do no uncomment and run all of them at the same time because if so, all the error curves will be drawn in the same plot
     
     # exp = Experiment(seeds, 0.2, 2, maze, 0.8, 0.5, 20)
     # exp.train()
@@ -252,21 +239,21 @@ if __name__ == '__main__':
     # exp3.train()
     # exp3.plot()
 
-    # exp = Experiment(seeds, 0.2, 2, maze, 0.8, 0.8, 200)
-    # exp.train()
-    # exp.plot()
+    # exp4 = Experiment(seeds, 0.2, 2, maze, 0.8, 0.8, 200)
+    # exp4.train()
+    # exp4.plot()
 
-    # exp1 = Experiment(seeds, 0.2, 4, maze, 0.8, 0.8, 200)
-    # exp1.train()
-    # exp1.plot()
+    # exp5 = Experiment(seeds, 0.2, 4, maze, 0.8, 0.8, 200)
+    # exp5.train()
+    # exp5.plot()
 
-    # exp2 = Experiment(seeds, 0.2, 6, maze, 0.8, 0.8, 200)
-    # exp2.train()
-    # exp2.plot()
+    # exp6 = Experiment(seeds, 0.2, 6, maze, 0.8, 0.8, 200)
+    # exp6.train()
+    # exp6.plot()
 
-    exp3 = Experiment(seeds, 0.2, 8, maze, 0.8, 0.2, 10)
-    exp3.train()
-    exp3.plot()
+    # exp7 = Experiment(seeds, 0.2, 8, maze, 0.8, 0.2, 10)
+    # exp7.train()
+    # exp7.plot()
 
 
 
